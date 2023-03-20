959518501d14bf5494f64a2f44252e958e947690
govindbose <govind@dqlabs.ai>
2023-03-20 20:04:07 +0530

    basic testing

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+import logging
 
 
 def error_message_details(error, error_details:sys):
@@ -11,9 +12,15 @@ def error_message_details(error, error_details:sys):
     
 class CustomException(Exception):
     def __init__(self, error_message, error_details:sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message = error_message_details(error_message, error_details=error_details)
         
     def __str__(self):
         return self.error_message
     
+# if __name__ == "__main__":
+#     try:
+#         error_happen = 100/0
+#     except Exception as e: 
+#         logging.info("Exception File")
+#         raise CustomException(e, sys)
